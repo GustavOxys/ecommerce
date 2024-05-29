b83d3b7974a37e1cb4cfe8ee0a1c504c04f93714
GustavOxys <gustavohzmdev@gmail.com>
2024-05-29 15:09:22 -0300

    Create models Order and ItemOrder, register in admin

--- order/models.py
+++ order/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        default='C',
+        max_length=1,
+        choices = (
+            ('A', 'Aproved'),
+            ('C', 'Created'),
+            ('U', 'Unsuccessful'),
+            ('P', 'Pending'),
+            ('S', 'Sent'),
+            ('F', 'Finished'),
+        )
+    )
+
+    def __str__(self):
+        return f'Order N. {self.pk}'
+
+
+class ItemOrder(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.CharField(max_length=255)
+    product_id = models.PositiveIntegerField()
+    variation = models.CharField(max_length=255)
+    variation_id = models.PositiveIntegerField()
+    price = models.FloatField()
+    price_promotional = models.FloatField(default=0)
+    quantity = models.PositiveIntegerField()
+    image = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return f'Item of {self.order}'
+
+
+    class Meta:
+        verbose_name = 'Order Item'
+        verbose_name_plural = 'Order Items'
